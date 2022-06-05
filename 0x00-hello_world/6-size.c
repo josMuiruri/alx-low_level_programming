ec668d8da6567f9af8e79f9a11ebe2abe6fdb346
josMuiruri <josemuiruri@gmail.com>
2022-06-05 16:00:36 -0700

    Task6. Size is not grandeur, and territory does not make a nation
    mandatory

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entrypoint
+ * main - entrypoint
  *
  * Return: Always 0 (success)
  *
@@ -30,7 +30,7 @@ int main(void)
 
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 
-	printf("Size of a float: %lu byte(s)\n", sizeof(afloat);
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 
 	return (0);
 
