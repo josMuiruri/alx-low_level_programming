f59b0769ee94a559771a3822601e5d5030cdc6b8
josMuiruri <josemuiruri@gmail.com>
2022-06-07 15:14:53 -0700

    Task0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,19 +3,20 @@
 /**
  * main - entry point
  *
- * prints out _putchar followed by a newline.
+ * Description: program that prints _putchar
  *
  * Return: always 0 (Success)
  */
 int main(void)
 {
-	char ch[8] = "_putchar";
-	int m;
-
-	for (m = 0; m < 8; m++)
-		_putchar(ch[m]);
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
