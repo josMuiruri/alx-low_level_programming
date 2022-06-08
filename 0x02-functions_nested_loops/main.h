deb7937e2578472853d0b634fbd226b1cd9765d8
josMuiruri <josemuiruri@gmail.com>
2022-06-08 14:21:32 -0700

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int_putchar(char c);
+void print_alphabet(void);
+void Print_alphabet_x10(void);
+int_islower(char c);
+int_isalpha(char c);
+int print_sign(int n);
+int_abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
