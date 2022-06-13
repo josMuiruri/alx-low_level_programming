6697b1e07610b27f903ac5debf3e05710177c140
josMuiruri <josemuiruri@gmail.com>
2022-06-13 14:08:42 -0700

    Task8. Arrays are not pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,24 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * puts_half - a function that prints half of a string, followed by a new line.
- * @str: An input string
+ * print_array - a fuction that  prints nelements of an array of integers,
+ * followed by a new line.
+ * @a: an input array
+ * @n: an input integer
  * Return: Nothing
  */
-void puts_half(char *str)
+void print_array(int *a, int n)
 {
-	int len = 0, i, n;
+	int i = 0;
 
-	while (str[len] != '\0')
-		len++;
-
-	if (len % 2 == 0)
-		n = len / 2;
-
-	else
-		n = (len + 1) / 2;
-
-	for (i = n; i < len; i++)
-		_putchar(str[i]);
-
-	_putchar('\n');
+	for (; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	putchar('\n');
 }
