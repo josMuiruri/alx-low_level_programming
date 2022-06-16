0d74de812c8f40073a3c9526dc3b06066fc1cd2b
josMuiruri <josemuiruri@gmail.com>
2022-06-16 13:29:44 -0700

    Task2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: string to copy to
+ * @src: string to copy from
+ * @n: maximum number of characters to copy from src
+ * Description:
+ * works exactly lik strncpy from <string.h>
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	char *new_dest = dest;
+
+	/* copy at most n charactersto dest */
+	while (*src && (i < n))
+	{
+		*dest++ = *src++;
+		i++;
+	}
+
+	/* ensure than n bytes are written */
+	while (i < n)
+	{
+		*dest++ = '\0';
+		i++;
+	}
+
+	return (new_dest);
+}
