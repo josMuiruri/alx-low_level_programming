157d7ff76984bba4e3d39adbe6203a477a4c14d7
josMuiruri <josemuiruri@gmail.com>
2022-06-16 16:01:04 -0700

    Task6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,71 +1,32 @@
 #include "main.h"
-
-int is_sep(char c);
-char cap_char(char c);
-
 /**
- * cap_string - capitalizes all words of a string
- * @str: string whose words to capitalize
- * Description:
- * Separators of words: space, tabulation, new line,
- * ,, ;, ., !, ?, ", (, ), {, and }
- * Return: pointer to str whose words have been capitalized
+ * cap_string - check description
+ * Description: capitalizes all words of a string
+ * @str: input
+ * Return: 0
  */
+
 char *cap_string(char *str)
 {
-	int i;
-	int sep_prev = 0;
+	int i, j;
+
+	char spe[13] = {' ', '\t', '\n', ',' , ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		/* check if the current character is a separator */
-		if (is_sep(str[i]))
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
+		for (j = 0; j < 13; j++)
 		{
-			sep_prev = 1
-		}
-		/* check if the previous character was a separator */
-		else if (sep_prev)
-		{
-			str[i] = cap_char(str[i]);
-			sep_prev = 0;
+			if (str[i] == spe[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+				}
+			}
 		}
 	}
-	/* capitalize the first character if string is non-empty */
-	if (i > 0 )
-		str[0] = cap_char(str[0]);
-
 	return (str);
-}
-/**
- * is_sep - cheks whether a given character is a separator
- * @c: character to be checked
- * Description:
- * Separators are: space, tabulation, new line,
- * ,, ;, ., !, ?, ", (, ), {, and  }
- */
-int is_sep(char c)
-{
-	char seps[] = {' ', '\t', '\n', ',', ';', '.', '?', '\"', '(', ')', '{', '}'};
-	int n = sizeof(seps) / sizeof(seps[0]);
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		if (c == seps[i])
-			return (1);
-	}
-
-	return (0);
-}
-/**
- * cap_char - gets the uppercase value of a character
- * @c: character whose uppercase value to find
- * Return: the uppercase value of c if c is in lowercase. Otherwise c
- */
-char cap_char(char c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (c -32);
-
-	return (c);
 }
