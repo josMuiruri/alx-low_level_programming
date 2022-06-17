9fc88753b66b28644acf9c9f48da3eeaf833e16e
josMuiruri <josemuiruri@gmail.com>
2022-06-16 18:29:47 -0700

    6-main.c

---
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -0,0 +1,17 @@
+#include "main.c"
+#include <stdio.h>
+/**
+ * main - check the code for main
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes. \nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	char *p;
+
+	p = cap_string(s);
+	printf("%s", p);
+	printf("%s", s);
+	return (0);
+}
