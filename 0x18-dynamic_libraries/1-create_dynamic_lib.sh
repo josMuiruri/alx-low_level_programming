a52f6fb5c1db5372d5358f7a0836418289992c8b
josmuiruri <josemuiruri51@gmail.com>
2023-09-23 01:23:50 +0300

    dynamic_library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-gcc -fPIC -c *.c
+
+gcc -c -Wall -Werror -fpic *.c
 gcc -shared -o liball.so *.o
+
+# Clean up object files
+rm *.o
+echo "Dynamic library liball.so created successfully!"
+# gcc -fPIC -c *.c
+# gcc -shared -Wl,-soname,liball.so -o liball.so *.o
